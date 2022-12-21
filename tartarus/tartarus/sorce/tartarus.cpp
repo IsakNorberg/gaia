b9e27a92d49883ee0f76e29d11ad3be46e30d7cf
Isak Uni <isakalv@gamil.com>
2022-12-22 00:31:07 +0100

    first

---
+++ tartarus/tartarus/sorce/tartarus.cpp
@@ -0,0 +1,11 @@
+// tartarus.cpp : Defines the functions for the static library.
+//
+#include "tartarus.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+
+bool test()
+{
+	return true;
+}
---
+++ test/sorce/test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+#include "tartarus.h"
+
+TEST(FirstTest, BasictTests) 
+{
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(test());
+}
+
